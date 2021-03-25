a6e8d71bef325ed4561dab8458a0251c4d02d0bd
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2021-03-25 19:58:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+var emojiDictionary = {
+  "🙂": "smiling",
+  "😟": "worried",
+  "🤩": "excited",
+  "❤": "love",
+  "🔥": "fire",
+  "😎": "cool",
+  "🙌": "hi-fi",
+  "👌": "okay",
+  "😳": "disbelief",
+  "😂": "tears of joy",
+  "👇🏻": "index pointing down",
+  "🤗": "Hugging Face",
+  "🥺": "Pleading Face",
+  "😉": "Winking Face",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
 
 function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "Not defined in the database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Emojificator</h1>
       </header>
+
+      <input onChange={emojiInputHandler} placeholder="Enter the emoji" />
+
+      <h2>{meaning}</h2>
+
+      <h3>Emojis We Know</h3>
+
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.7rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
